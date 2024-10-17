00784e4e84cc35897550e9bdfaa9b342cccdbdd4
CodingManoj <CodingManoj@gmail.com>
2024-10-16 23:07:34 -0400

    Kubernetes AutoPush

--- eks/addOns.tf
+++ eks/addOns.tf
@@ -3,4 +3,7 @@ resource "aws_eks_addon" "example" {
   depends_on   = [aws_eks_cluster.example]
   cluster_name = aws_eks_cluster.example.name
   addon_name   = "vpc-cni"
+  configuration_values = jsonencode({
+    enableNetworkPolicy = true
+  })
 }
