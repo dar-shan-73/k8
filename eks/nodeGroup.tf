09269f1dea15c9194be3275a4d27afa3a559c9f5
CodingManoj <CodingManoj@gmail.com>
2024-10-29 18:02:07 -0400

    Kubernetes AutoPush

--- eks/nodeGroup.tf
+++ eks/nodeGroup.tf
@@ -134,7 +134,7 @@ resource "aws_eks_identity_provider_config" "oidc" {
   oidc {
     client_id                     = local.eks_client_id
     identity_provider_config_name = "iam-oidc"
-    issuer_url                    = aws_eks_cluster.example.identity[0].oidc[0].issuer
+    issuer_url                    = aws_eks_cluster.example.url
   }
 }
 
