df08215c3f481be6289cc3a295873ac540615035
CodingManoj <CodingManoj@gmail.com>
2024-10-29 09:53:16 -0400

    Kubernetes AutoPush

--- sample.tf
+++ sample.tf
@@ -1,6 +1,6 @@
 data "external" "myjson" {
   program = [
-    "kubergrunt eks oidc-thumbprint --issuer-url https://oidc.eks.us-east-1.amazonaws.com/id/0A0D0750871AD0E848F90F4ECC6ACBFF",
+    "kubergrunt", "eks", "oidc-thumbprint", "--issuer-url", "https://oidc.eks.us-east-1.amazonaws.com/id/0A0D0750871AD0E848F90F4ECC6ACBFF",
   ]
 }
 output "data" {
